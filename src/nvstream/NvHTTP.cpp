cdcafef1c6a5080c0fc86a7cd8b8b50ed5575acd
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2015-06-24 22:17:13 -0700

    Add stubs for NvHTTP

---
+++ src/nvstream/NvHTTP.cpp
@@ -0,0 +1,47 @@
+/*
+ *      Copyright (C) 2015 Anthony Miyaguchi
+ *      Copyright (C) 2015 Team XBMC
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+ #include "NvHTTP.h"
+
+std::string NvHTTP::getServerInfo(std::string uid)
+{
+	return "";
+}
+
+PairState NvHTTP::getPairState(std::string serverInfo)
+{
+	return PairState::FAILED;
+}
+
+int NvHTTP::getCurrentGame(std::string serverInfo)
+{
+	return -1;
+}
+
+PairState NvHTTP::pair(std::string pin)
+{
+	return PairState::FAILED;
+}
+
+std::string NvHTTP::openHTTPConnection(std::string url, bool enableReadTimeout)
+{
+	return "";
+}
+
+
---
+++ src/nvstream/PairingManager.h
@@ -0,0 +1,34 @@
+/*
+ *      Copyright (C) 2015 Anthony Miyaguchi
+ *      Copyright (C) 2015 Team XBMC
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+ #pragma once
+
+enum class PairState
+{
+	NOT_PAIRED,
+	PAIRED,
+	PIN_WRONG,
+	FAILED
+};
+
+ class PairingManager
+ {
+ public:
+ 	PairingManager() {}
+ };
