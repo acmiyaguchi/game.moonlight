e5361a36109d1b12ee53b469a0860fefd23f0df5
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2015-07-07 22:23:11 -0700

    Add stubs for certification generation

---
+++ src/nvstream/CertKeyPair.cpp
@@ -0,0 +1,44 @@
+/*
+ *      Copyright (C) 2015 Anthony Miyaguchi
+ *      Copyright (C) 2015 Team XBMC
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+#include "CertKeyPair.h"
+#include <openssl/pem.h>
+#include <openssl/conf.h>
+#include <openssl/pkcs12.h>
+
+using namespace MOONLIGHT;
+
+CertKeyPair::CertKeyPair()
+{
+}
+
+CertKeyPair::~CertKeyPair()
+{
+
+}
+
+void CertKeyPair::generate()
+{
+
+}
+
+void CertKeyPair::save(std::string certFile, std::string p12File, std::string keyPairFile)
+{
+
+}
---
+++ src/nvstream/CertKeyPair.h
@@ -0,0 +1,41 @@
+/*
+ *      Copyright (C) 2015 Anthony Miyaguchi
+ *      Copyright (C) 2015 Team XBMC
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+#pragma once
+
+#include <openssl/x509v3.h>
+#include <openssl/pkcs12.h>
+#include <string>
+
+namespace MOONLIGHT
+{
+  class CertKeyPair
+  {
+  public:
+    CertKeyPair();
+    virtual ~CertKeyPair();
+    void generate();
+    void save(std::string certFile, std::string p12File, std::string keyPairFile);
+  
+  private:
+    X509*     m_x509;
+    EVP_PKEY* m_pkey;
+    PKCS12*   m_p12;
+  };
+}
