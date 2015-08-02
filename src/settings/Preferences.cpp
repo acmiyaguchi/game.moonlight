61e3290d9dfb09b540737d29c8a21ffe2f612402
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2015-08-02 11:50:53 -0700

    Save unique id to disk

--- src/settings/Preferences.cpp
+++ src/settings/Preferences.cpp
@@ -20,10 +20,16 @@
 #include "Preferences.h"
 #include <cstdlib>
 #include <cstdio>
+#include <cstring>
 #include <ctime>
 #include <openssl/rand.h>
 
 using namespace MOONLIGHT;
+
+#define UID_CHARS 16
+namespace {
+  const char *uid_filename = "uniqueid.dat";
+}
 
 Preferences::Preferences()
 {
@@ -45,12 +51,23 @@ Preferences::~Preferences()
 
 void Preferences::init(ResolutionType res, bool fullscreen)
 {
-  char buf[20];
-  unsigned char unique_data[8];
-  RAND_bytes(unique_data, 8);
-  for (int i = 0; i < 8; i++) {
-    sprintf(buf + (i * 2), "%02x", unique_data[i]);
+  char buf[UID_CHARS+1];
+  FILE *fd = fopen(uid_filename, "r");
+  if (fd == NULL) {
+    unsigned char unique_data[8];
+    RAND_bytes(unique_data, 8);
+    for (int i = 0; i < 8; i++) {
+      sprintf(buf + (i * 2), "%02x", unique_data[i]);
+    }
+    buf[UID_CHARS] = 0;
+    fd = fopen(uid_filename, "w");
+    fwrite(buf, UID_CHARS, 1, fd);
   }
+  else {
+    fread(buf, UID_CHARS, 1, fd);
+    buf[UID_CHARS] = 0;
+  }
+  fclose(fd);
 
   m_resolution = new Resolution(res);
   m_bitrate = m_resolution->getBitrate();
